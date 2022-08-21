979684b0a6df48114f3cb5689a456fc67160ac47
kerryoncoding <103139429+kerryoncoding@users.noreply.github.com>
2022-08-21 11:05:09 -0400

    cleaning up code

--- src/index.js
+++ src/index.js
@@ -2,71 +2,49 @@
 // have current day and time be displayed when page loads
 
 function formatDate(date) {
-//Day of week
-
-let day = date.getDay();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let dayOfWeek = (days[day]);
-
-//time minutes
-let timeMinutes = date.getMinutes();
-if (timeMinutes < 10) {
-  timeMinutes = `0${timeMinutes}`;
-}
-
-//time of day converted to 12-Hour with AM/PM
-let timeHour = date.getHours();
-if (timeHour === 12) {
-  return `${dayOfWeek} ${timeHour}:${timeMinutes} PM`;
-} else { if (timeHour === 0) {
-  return `${dayOfWeek} 12:${timeMinutes} AM`;
-} else {
-  if (timeHour > 12) { 
-  timeHour = timeHour - 12;
-  return `${dayOfWeek} ${timeHour}:${timeMinutes} PM`;
+  //Day of week
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let dayOfWeek = (days[day]);
+  //time minutes
+  let timeMinutes = date.getMinutes();
+  if (timeMinutes < 10) {
+    timeMinutes = `0${timeMinutes}`;
+  }
+  //time of day converted to 12-Hour with AM/PM
+  let timeHour = date.getHours();
+  if (timeHour === 12) {
+    return `${dayOfWeek} ${timeHour}:${timeMinutes} PM`;
+  } else { if (timeHour === 0) {
+    return `${dayOfWeek} 12:${timeMinutes} AM`;
   } else {
-  return `${dayOfWeek} ${timeHour}:${timeMinutes} AM`;
+    if (timeHour > 12) { 
+    timeHour = timeHour - 12;
+    return `${dayOfWeek} ${timeHour}:${timeMinutes} PM`;
+    } else {
+    return `${dayOfWeek} ${timeHour}:${timeMinutes} AM`;
+    }
+  }
   }
 }
-}
-}
-
-let timeNow = new Date();
-document.querySelector(".current-day-time").innerHTML = formatDate(timeNow);
 
 
-//get current location name
-//function getMyLocation(position) {
-//  let latitude = position.coords.latitude;
-//  let longitude = position.coords.longitude;
-//  let apiKey = "ca47e9200d90350ad07692b8ce034ca3";
-//  let apiUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-//  axios.get(apiUrl).then(showCurrentName);
-//}
-
-//function showCurrentName(currentCityName) {
-//   let cityName = (currentCityName.data[0].name);
-//   searchCity(cityName);
-//}
 
 function handleSubmit(event) {
   event.preventDefault();
+  celsious.classList.add("active");
+  fahrenheit.classList.remove("active");
   let city = document.querySelector("#input-city-name").value;
-  searchCity(city);
-}
-
-function searchCity(updateCity) {
   let apiKey = "ca47e9200d90350ad07692b8ce034ca3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${updateCity}&APPID=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(`${apiUrl}`).then(showCurrentWeather);
 }
 
@@ -100,43 +78,42 @@ function showCurrentWeather(response) {
   let iconCode = response.data.weather[0].icon;
   document.querySelector(".current-weather-image").setAttribute("src", `images/${iconCode}.png`);
 
-  getForecast(response.data.coord);
-  
+  getForecast(response.data.coord); 
 }
-
-
-//function getCurrentLocation(event) {
-//  event.preventDefault();
-//  navigator.geolocation.getCurrentPosition(getMyLocation);
-// }
 
 
 function unitsFahrenheit() {
     celsious.classList.remove("active");
     fahrenheit.classList.add("active");
     document.querySelector(".current-temperature-value").innerHTML = Math.round(celsiousTemperature * 9/5) + 32;
+    document.querySelector(".#forecast").innerHTML = Math.round(futureCelsiousTemperatureMax * 9/5) + 32;
+    document.querySelector(".#forecast").innerHTML = Math.round(futureCelsiousTemperatureMin * 9/5) + 32;
 }
 
 function unitsCelsious() {
-    celsious.classList.add("active");
-    fahrenheit.classList.remove("active");
-    document.querySelector(".current-temperature-value").innerHTML = Math.round(celsiousTemperature);
+  celsious.classList.add("active");
+  fahrenheit.classList.remove("active");
+  document.querySelector(".current-temperature-value").innerHTML = Math.round(celsiousTemperature);
 }
 
 function formatFutureDate(timestamp) {
-    let futureDate = new Date(timestamp * 1000);
-    let futureDay = futureDate.getDay();
-    let futureDayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    return futureDayNames[futureDay];
+  let futureDate = new Date(timestamp * 1000);
+  let futureDay = futureDate.getDay();
+  let futureDayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return futureDayNames[futureDay];
 }
 
 function displayForecast(response) {
+ 
   let futureDates = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
   futureDates.forEach(function(futureDay, index) {
     if (index < 5) { 
+      let futureCelsiousTemperatureMax = futureDay.temp.max;
+      let futureCelsiousTemperatureMin = futureDay.temp.min; 
+
       forecastHTML = forecastHTML +
            `<div class="col-2 card-spacing">
               <div class="card" style="width: 7rem">
@@ -147,8 +124,8 @@ function displayForecast(response) {
                           alt="weather image"
                           class="future-image"
                         />
-                      <div><span class="future-high">${Math.round(futureDay.temp.max)}°</span>
-                      <span class="future-low">/${Math.round(futureDay.temp.min)}°</span></div>
+                      <div><span class="future-high">${Math.round(futureCelsiousTemperatureMax)}°</span>
+                      <span class="future-low">/${Math.round(futureCelsiousTemperatureMin)}°</span></div>
                   </div>
               </div>
             </div>`;
@@ -166,52 +143,19 @@ document.querySelector("#celsious").addEventListener("click", unitsCelsious);
 
 let celsiousTemperature = null;
 
+let city = "New York";
+document.querySelector(".input-city").addEventListener("submit", handleSubmit);
+
+
+let timeNow = new Date();
+document.querySelector(".current-day-time").innerHTML = formatDate(timeNow);
 
 //submit button
-let cityForm = document.querySelector(".input-city");
-cityForm.addEventListener("submit", handleSubmit);
-
-//current button
-//let currentButton = document.querySelector("#current-button");
-//currentButton.addEventListener("click", getCurrentLocation);
 
 
-//default page search
-searchCity("New York");
-
-
-
-
-
-
-
-
-// toggle between F and C temperatures (not needing to use math)
-
-//function changeUnitsF() {
-//  let updateToF = document.querySelector("#f-unit");
-//  updateToF.innerHTML = "<strong>°F</strong>";
-//  let updateToC = document.querySelector("#c-unit");
-//  updateToC.innerHTML = "°C"
-//  let convertTemp = document.querySelector(".current-temperature-value");
-//  convertTemp.innerHTML = "76";
-//}
-
-//function changeUnitsC() {
-//  let updateToC = document.querySelector("#c-unit");
-//  updateToC.innerHTML = "<strong>°C</strong>";
-//  let updateToF = document.querySelector("#f-unit");
-//  updateToF.innerHTML = "°F"
-//    let convertTemp = document.querySelector(".current-temperature-value");
-//  convertTemp.innerHTML = "24";
-//}
-
-//let fahrenheit = document.querySelector("#f-unit");
-//let celsius = document.querySelector("#c-unit");
-
-//fahrenheit.addEventListener("click", changeUnitsF);
-
-//celsius.addEventListener("click", changeUnitsC);
+let apiKey = "ca47e9200d90350ad07692b8ce034ca3";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=metric`;
+axios.get(`${apiUrl}`).then(showCurrentWeather);
 
 
 
